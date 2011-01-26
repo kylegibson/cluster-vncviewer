b2b2f387059d92bceabcfe263a550a8b981a16c2
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-01-26 00:21:47 -0500

    added MIT license information

--- cluster-vncviewer.py
+++ cluster-vncviewer.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python
-# Cluster VNC viewer
+#
+# The MIT License
+# 
+# Copyright (c) 2011 Kyle Gibson
 
-version = "0.1"
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
-import sys
+VERSION = "0.1"
+COPYRIGHT = "(c) 2011 Kyle Gibson"
 
 import pygtk
 pygtk.require("2.0")
@@ -13,11 +33,11 @@ import gtk.glade
 import gtkvnc
 import gobject
 import os
+import sys
 
 class VNCViewer:
 	def __init__(self, info):
 		info = info.split(":")
-		print info
 		self.hostname = info[0]
 		self.port = info[1]
 		self.password = info[2]
@@ -125,7 +145,8 @@ class VNCViewer:
 		self.window_label.set_markup ("<big><b>%s</b></big>" % title)
 		self.window.show_all()
 		self.window_toolbar_note.hide_all()
-		self.window.resize (vnc.get_width(), vnc.get_height())
+		self.window.resize (1200, 600)
+		#self.window.resize (vnc.get_width(), vnc.get_height())
 		vnc.grab_focus()
 
 	def vnc_disconnected(src, vnc, self):
@@ -137,6 +158,7 @@ def wait():
 		gtk.main_iteration()
 
 if __name__ == "__main__":
+	print "Cluster VNC", VERSION, COPYRIGHT
 	VNCViewer.open_windows = 0
 
 	instances = []
