3943c597947b30cfc5f3cb5126ba1bec646ca624
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-01-26 00:23:19 -0500

    fixed license

--- cluster-vncviewer.py
+++ cluster-vncviewer.py
@@ -3,7 +3,7 @@
 # The MIT License
 # 
 # Copyright (c) 2011 Kyle Gibson
-
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
